f74bdadc32473142354dd19236f9c8d9822ad708
melizamov <melizamov94@gmail.com>
2022-04-14 15:54:32 +0300

    update

--- src/test/java/Pages/Products.java
+++ src/test/java/Pages/Products.java
@@ -37,50 +37,15 @@ public class Products {
         return Integer.parseInt(shoppingCartCounter.getText());
     }
 
-        public boolean HamburgerMenu() {
-            WebElement HamburgerMenuButton = driver.findElement(By.id("react-burger-menu-btn"));
-            return HamburgerMenuButton.isDisplayed();
-        }
+    public boolean HamburgerMenu() {
+        WebElement HamburgerMenuButton = driver.findElement(By.id("react-burger-menu-btn"));
+        return HamburgerMenuButton.isDisplayed();
+    }
 
-    public ShoppingCart openShoppingCartPage(){
+    public ShoppingCart openShoppingCartPage() {
         shoppingCartLink.click();
 
         return new ShoppingCart(driver);
     }
-
-//        public boolean areProductsSortedByPriceAsc () {
-//            List<WebElement> priceHolders = driver.findElements(By.className("inventory_item_price"));
-//
-//            Double price = 0.0;
-//            boolean areSorted = true;
-//            for (int i = 0; i < priceHolders.size(); i++) {
-//                String productPriceStr = priceHolders.get(i).getText().replace("$", "");
-//
-//                // convert into Double
-//                double productPrice = Double.parseDouble(productPriceStr);
-//
-//                if (productPrice < price) {
-//                    areSorted = false;
-//                    break;
-//                }
-//
-//                price = productPrice;
-//            }
-//
-//            return areSorted;
-//        }
-
-//        public void sortByPriceAsc () {
-//            FluentWait fluentWait = new FluentWait(driver)
-//                    .withTimeout(Duration.ofSeconds(10))
-//                    .pollingEvery(Duration.ofSeconds(1)) // how often it will be checked for the presence of the element
-//                    .ignoreAll(Collections.singleton(NoSuchElementException.class));
-//
-//            WebElement lowToHighPriceOption = driver.findElement(By.cssSelector("[value=lohi]"));
-//
-//            fluentWait.until(ExpectedConditions.elementToBeClickable(lowToHighPriceOption));
-//            lowToHighPriceOption.click();
-//        }
-//    }
-    }
+}
 
